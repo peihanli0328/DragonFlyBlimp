a2838d5abce40ff5a459194309a886b48a76b4b1
Peihan Li <peihanli2016@yahoo.com>
2023-10-06 17:46:34 -0400

    Mapped the servo with angles give, synchronized two servos. The controller mapping and the interrupt still need to be addressed.

--- servo/servo.ino
+++ servo/servo.ino
@@ -3,31 +3,43 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  150
-#define SERVOMAX  600
+// #define SERVOMIN  110
+// #define SERVOMAX  520
+#define SERVOMIN  100
+#define SERVOMAX  450
 #define SERVO_FREQ 50
-
+int satrting_servoAngle = ((SERVOMAX-SERVOMIN)/2)+SERVOMIN; 
+int servoAngle;
 void setup() {
   Serial.begin(115200);
-  Serial.println("2 channel Servo test!");
+  // Serial.println("2 channel Servo test!");
 
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);
-  delay(10);
+  // pwm.setPWM(0, 0, map(90, 0, 180, SERVOMIN, SERVOMAX));
+  // pwm.setPWM(4, 0, map(90, 180, 0, SERVOMIN, SERVOMAX));
+  pwm.setPWM(3, 0,satrting_servoAngle);
+  pwm.setPWM(4, 0,satrting_servoAngle);
+  delay(100);
+  Serial.println("2 channel Servo test!");
+
 }
 
 void loop() {
-  // for rotating at same time
-  for (int angle = 0; angle <= 180; angle++) {
-    pwm.setPWM(0, 0, map(angle, 0, 180, SERVOMIN, SERVOMAX));
-    pwm.setPWM(4, 0, map(angle, 0, 180, SERVOMIN, SERVOMAX));
-    delay(15); 
-  }
+  if (Serial.available()) {
+    String angleStr = Serial.readStringUntil('\n');  // Read the incoming data until newline character
+    int newAngle = angleStr.toInt();  // Convert the string to integer
+    Serial.print("Setting servo time to: ");
+    servoAngle = newAngle;
+    Serial.println(servoAngle);
+    }
+  
 
-  for (int angle = 180; angle >= 0; angle--) {
-    pwm.setPWM(0, 0, map(angle, 0, 180, SERVOMIN, SERVOMAX));
-    pwm.setPWM(4, 0, map(angle, 0, 180, SERVOMIN, SERVOMAX));
-    delay(15); 
-  }
+  // Update the servo position
+  // pwm.setPWM(3, 0, servoAngle);
+  pwm.setPWM(0, 0, map(servoAngle, 0, 180, SERVOMIN, SERVOMAX));
+  pwm.setPWM(4, 0, map(servoAngle, 180, 0, SERVOMIN, SERVOMAX));
+  delay(10);
+  
 }
